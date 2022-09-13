73dc4395fd3e48cdeda586f6c548c89888ee1dbb
Muhammad Fahmi Albaihaqi <fhmalba@gmail.com>
2022-09-13 14:34:47 +0700

    add roman converter

---
+++ src/js/roman.js
@@ -0,0 +1,43 @@
+let elRoman = document.getElementById("roman");
+
+let romanNumeric = [];
+
+function generate() {
+  let roman = elRoman.value;
+  document.getElementById("arabic").value = romanToDecimal(roman);
+}
+
+function value(r) {
+  if (r == "I") return 1;
+  if (r == "V") return 5;
+  if (r == "X") return 10;
+  if (r == "L") return 50;
+  if (r == "C") return 100;
+  if (r == "D") return 500;
+  if (r == "M") return 1000;
+  return -1;
+}
+
+function romanToDecimal(roman) {
+  let str = roman.toUpperCase();
+  var res = 0;
+
+  for (i = 0; i < str.length; i++) {
+    var s1 = value(str.charAt(i));
+
+    if (i + 1 < str.length) {
+      var s2 = value(str.charAt(i + 1));
+
+      if (s1 >= s2) {
+        res = res + s1;
+      } else {
+        res = res + s2 - s1;
+        i++;
+      }
+    } else {
+      res = res + s1;
+    }
+  }
+
+  return res;
+}
